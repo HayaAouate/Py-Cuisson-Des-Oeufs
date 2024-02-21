be3b58fdf47082d0aa67332473ecff94951412d8
HayaAouate <145683041+HayaAouate@users.noreply.github.com>
2024-02-21 11:09:19 +0100

    Gestion des erreurs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import time
 
-
 def pointille():
     for i in range(0, 11):
         time.sleep(1)
@@ -13,17 +12,43 @@ def cuisson_voulu(temps):
             print(f"Temps restant : {min}:{sec}", end="")
             pointille()
             print()
-    print("T")
+        print("T")
     return ()
+def cuisson_en_cours():
+       print("Cuisson en cours : ", end="")
+       pointille()
+       print()
+       return ()
 
 print("Option de cuisson")
 a = "a - Oeuf à la coque : 3min "
 b = "b - Oeuf mollets: 6min "
 c = "c - Oeuf durs : 9min "
-choix = input(f"{a} \n{b}\n{c} \nVotre choix : ")
-if choix == a:
-    cuisson_voulu(3)
-elif choix == b:
-    cuisson_voulu(6)
-else:
-    cuisson_voulu(9)
+
+# Boucle while pour demander à l'utilisateur de saisir un choix valide
+while True:
+    choix = input(f"{a} \n{b}\n{c} \nVotre choix : ")
+
+    # Vérifier si le choix de l'utilisateur est valide
+    if choix in ["a", "b", "c"]:
+        try:
+            if choix == "a":
+                cuisson_en_cours()
+                cuisson_voulu(3)
+
+            elif choix == "b":
+                cuisson_en_cours()
+                cuisson_voulu(6)
+            else:
+                cuisson_en_cours()
+                cuisson_voulu(9)
+        except ValueError:
+            print(f"Erreur : veuillez saisir une lettre ")
+            break
+    elif choix.isdigit():
+        print('Veuillez saisir une lettre')
+    else:
+        print(f"Erreur : veuillez saisir entre a, b ou c ")
+
+
+
